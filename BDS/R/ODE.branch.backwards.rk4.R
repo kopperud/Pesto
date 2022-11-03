406e7d61646131dd1c95626872510cb06a00459f
Bjorn Tore Kopperud <kopperud@protonmail.com>
2022-11-03 14:02:45 +0100

    clean up ODE solver fs

--- BDS/R/ODE.branch.backwards.rk4.R
+++ BDS/R/ODE.branch.backwards.rk4.R
@@ -15,12 +15,6 @@
 #'
 #' @examples
 backwards <- function(lambda, mu, eta, tstart, tend, E0, D0, ntimes = 100){
-  backwardsED <- function(t, y, parms) {
-    E <- parms[["mu"]] - (parms[["lambda"]] + parms[["mu"]] + parms[["eta"]]) * y[1:parms[["k"]]] + parms[["lambda"]] * y[1:parms[["k"]]]^2 + (parms[["eta"]]/(parms[["k"]]-1)) * (sum(y[1:parms[["k"]]]) - y[1:parms[["k"]]])
-    D <- - (parms[["lambda"]] + parms[["mu"]] + parms[["eta"]]) * y[(parms[["k"]]+1):(2*parms[["k"]])] + 2 * parms[["lambda"]] * y[(parms[["k"]]+1):(2*parms[["k"]])] * y[1:parms[["k"]]] + (parms[["eta"]] / (parms[["k"]] - 1)) * (sum(y[(parms[["k"]]+1):(2*parms[["k"]])]) - y[(parms[["k"]]+1):(2*parms[["k"]])])
-
-    return(list(c("E" = E, "D" = D)))
-  }
   k <- length(lambda)
   parameters <- list("lambda" = lambda, "mu" = mu, "eta" = eta, "k" = k)
   yini <- c("E" = E0, "D" = D0)
@@ -30,40 +24,10 @@ backwards <- function(lambda, mu, eta, tstart, tend, E0, D0, ntimes = 100){
   return(as.data.frame(out))
 }
 
-# branch.prob.backwards.rk4 <- function(lambda, mu, eta, TIME, D.init, E.init){
-#   backwards <- function(t, y, parms) {
-#     E1 <-  parms[[2]][1] - (parms[[1]][1]+parms[[2]][1]+parms[[3]])*y[1] + parms[[1]][1] * y[1]^2 + parms[[3]] * y[2]
-#     E2 <-  parms[[2]][2] - (parms[[1]][2]+parms[[2]][2]+parms[[3]])*y[2] + parms[[1]][2] * y[2]^2 + parms[[3]] * y[1]
-#
-#     D1 <- - (parms[[1]][1]+parms[[2]][1]+parms[[3]])*y[3] + 2*parms[[1]][1]*y[3] * y[1] + parms[[3]] * y[4]
-#     D2 <- - (parms[[1]][2]+parms[[2]][2]+parms[[3]])*y[4] + 2*parms[[1]][2]*y[4] * y[2] + parms[[3]] * y[3]
-#
-#     return(list(c(E1, E2, D1, D2)))
-#   }
-#
-#   parameters <- list("lambda" = lambda, "mu" = mu, "eta" = eta)
-#   yini <- c(E1 = E.init[1],
-#             E2 = E.init[2],
-#             D1 = D.init[1],
-#             D2 = D.init[2])
-#   times <- seq(0, TIME, by = 0.001)
-#   #times <- c(0, TIME)
-#
-#   out <- deSolve::rk4(y = yini, times = times, func = backwards, parms = parameters)
-#   return(as.data.frame(out))
-# }
-#
-# extinction.rk4 <- function(lambda, mu, eta, tstart, tend, u0, ntimes = 100){
-#   backwards <- function(t, y, parms) {
-#     E <- parms[[2]] - (parms[[1]] + parms[[2]] + parms[[3]]) * y + parms[[1]] * y^2 + (parms[[3]]/(parms[["k"]]-1)) * (sum(y) - y)
-#     return(list(E))
-#   }
-#   k <- length(lambda)
-#   parameters <- list("lambda" = lambda, "mu" = mu, "eta" = eta, "k" = k)
-#   yini <- c("E" = u0)
-#
-#   times <- seq(tstart, tend, length.out = ntimes)
-#   out <- deSolve::rk4(y = yini, times = times, func = backwards, parms = parameters)
-#   return(as.data.frame(out))
-# }
+backwardsED <- function(t, y, parms) {
+  E <- parms[["mu"]] - (parms[["lambda"]] + parms[["mu"]] + parms[["eta"]]) * y[1:parms[["k"]]] + parms[["lambda"]] * y[1:parms[["k"]]]^2 + (parms[["eta"]]/(parms[["k"]]-1)) * (sum(y[1:parms[["k"]]]) - y[1:parms[["k"]]])
+  D <- - (parms[["lambda"]] + parms[["mu"]] + parms[["eta"]]) * y[(parms[["k"]]+1):(2*parms[["k"]])] + 2 * parms[["lambda"]] * y[(parms[["k"]]+1):(2*parms[["k"]])] * y[1:parms[["k"]]] + (parms[["eta"]] / (parms[["k"]] - 1)) * (sum(y[(parms[["k"]]+1):(2*parms[["k"]])]) - y[(parms[["k"]]+1):(2*parms[["k"]])])
+
+  return(list(c("E" = E, "D" = D)))
+}
 
--- BDS/R/ODE.branch.forwards.rk4.R
+++ BDS/R/ODE.branch.forwards.rk4.R
@@ -1,17 +1,4 @@
 forwards <- function(lambda, mu, eta, tstart, tend, E0, D0, F0, ntimes = 100){
-  forwardsEDF <- function(t, y, parms) {
-    lambda <- parms[["lambda"]]
-    mu <- parms[["mu"]]
-    eta <- parms[["eta"]]
-    k <- parms[["k"]]
-
-    E <- (-1) * (mu - (lambda + mu + eta) * y[1:k] + lambda * y[1:k]^2 + (eta/(k-1)) * (sum(y[1:k]) - y[1:k]))
-    D <- (-1) * (- (lambda + mu + eta) * y[(k+1):(2*k)] + 2 * lambda * y[(k+1):(2*k)] * y[1:k] + (eta / (k-1)) * (sum(y[(k+1):(2*k)]) - y[(k+1):(2*k)]))
-    F1 <- (-1) * (- (lambda + mu + eta) * y[(2*k+1):(3*k)] + 2 * lambda * y[(2*k+1):(3*k)] * y[1:k] + (eta / (k-1)) * (sum(y[(2*k+1):(3*k)]) - y[(2*k+1):(3*k)]))
-
-    return(list(c("E" = E, "D" = D, "F" = F1)))
-  }
-
   k <- length(lambda)
   parameters <- list("lambda" = lambda, "mu" = mu, "eta" = eta, "k" = k)
   yini <- c("E" = E0, "D" = D0, "F" = F0)
@@ -21,32 +8,15 @@ forwards <- function(lambda, mu, eta, tstart, tend, E0, D0, F0, ntimes = 100){
   return(as.data.frame(out))
 }
 
-# branch.prob.forwards.rk4 <- function(lambda, mu, eta, TIME, F.init, D.init, E.init){
-#   forwards <- function(t, y, parms) {
-#     E1 <-  parms[[2]][1] - (parms[[1]][1]+parms[[2]][1]+parms[[3]])*y[1] + parms[[1]][1] * y[1]^2 + parms[[3]] * y[2]
-#     E2 <-  parms[[2]][2] - (parms[[1]][2]+parms[[2]][2]+parms[[3]])*y[2] + parms[[1]][2] * y[2]^2 + parms[[3]] * y[1]
-#
-#     D1 <- - (parms[[1]][1]+parms[[2]][1]+parms[[3]])*y[3] + 2*parms[[1]][1]*y[3] * y[1] + parms[[3]] * y[4]
-#     D2 <- - (parms[[1]][2]+parms[[2]][2]+parms[[3]])*y[4] + 2*parms[[1]][2]*y[4] * y[2] + parms[[3]] * y[3]
-#
-#     F1 <- (-1) * - (parms[[1]][1]+parms[[2]][1]+parms[[3]])*y[5] + 2*parms[[1]][1]*y[5] * y[1] + parms[[3]] * y[6]
-#     F2 <- (-1) * - (parms[[1]][2]+parms[[2]][2]+parms[[3]])*y[6] + 2*parms[[1]][2]*y[6] * y[2] + parms[[3]] * y[5]
-#
-#     return(list(c(E1, E2, D1, D2, F1, F2)))
-#   }
-#
-#   parameters <- list("lambda" = lambda, "mu" = mu, "eta" = eta)
-#   yini <- c(E1 = E.init[1],
-#             E2 = E.init[2],
-#             D1 = D.init[1],
-#             D2 = D.init[2],
-#             F1 = F.init[1],
-#             F2 = F_init[2])
-#   times <- seq(0, TIME, by = 0.001)
-#   #times <- c(0, TIME)
-#
-#   out <- deSolve::rk4(y = yini, times = times, func = forwards, parms = parameters)
-#   return(as.data.frame(out))
-# }
+forwardsEDF <- function(t, y, parms) {
+  lambda <- parms[["lambda"]]
+  mu <- parms[["mu"]]
+  eta <- parms[["eta"]]
+  k <- parms[["k"]]
 
+  E <- (-1) * (mu - (lambda + mu + eta) * y[1:k] + lambda * y[1:k]^2 + (eta/(k-1)) * (sum(y[1:k]) - y[1:k]))
+  D <- (-1) * (- (lambda + mu + eta) * y[(k+1):(2*k)] + 2 * lambda * y[(k+1):(2*k)] * y[1:k] + (eta / (k-1)) * (sum(y[(k+1):(2*k)]) - y[(k+1):(2*k)]))
+  F1 <- (-1) * (- (lambda + mu + eta) * y[(2*k+1):(3*k)] + 2 * lambda * y[(2*k+1):(3*k)] * y[1:k] + (eta / (k-1)) * (sum(y[(2*k+1):(3*k)]) - y[(2*k+1):(3*k)]))
 
+  return(list(c("E" = E, "D" = D, "F" = F1)))
+}
