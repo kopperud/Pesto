96816f73424b5c31ab3f412a461702eff8321d82
Bjorn Tore Kopperud <kopperud@protonmail.com>
2022-11-03 17:44:57 +0100

    add time bench mark of two implementations

---
+++ scripts/bears.jl
@@ -0,0 +1,67 @@
+using RCall
+using Distributions
+using StatsPlots
+using Diversification
+
+##############################
+##
+##   Load the data files
+##
+###############################
+treefile = "data/bears.tre"
+datafile = ""
+phy = readtree(treefile)
+num_total_species = 8
+ρ = length(phy[:tip_label]) / num_total_species
+data = make_SSEdata(phy, datafile, ρ; include_traits = false)
+
+##############################
+##
+##   Set up the model
+##
+###############################
+λ = [0.05, 0.1, 0.15, 0.2]
+μ = 0.5 .* λ
+
+η = 0.1
+model = SSEconstant(λ, μ, η)
+
+
+## Calculate the backwards-forwards pass equations
+Ds, Fs = backwards_forwards_pass(model, data; verbose = true) 
+res = calculate_tree_rates(data, model, Ds, Fs; verbose = false);
+
+average_node_rates = res["average_node_rates"]
+
+phy = Dict("edge" => data.edges,
+      "tip.label" => data.tiplab,
+      "Nnode" => length(data.tiplab)-1,
+     "edge.length" => data.branch_lengths)
+
+lambda_average = average_node_rates["λ"]
+
+@rput lambda_average
+@rput phy
+R"""
+library(ape)
+library(ggtree)
+library(tidytree)
+library(ggplot2)
+library(dplyr)
+
+class(phy) <- "phylo"
+th <- max(node.depth.edgelength(phy))
+
+df1 <- tibble("node" = 1:max(phy$edge),
+            "Speciation rate" = lambda_average)
+x <- as_tibble(phy)
+
+phydf <- merge(x, df1, by = "node")
+td_phy <- as.treedata(phydf)
+
+p1a <- ggtree(td_phy, aes(color = `Speciation rate`)) +
+    geom_tiplab(size = 8) +
+    theme(legend.position = c(0.2, 0.8)) +
+    xlim(c(0.0, th + 10)) 
+""";
+
---
+++ scripts/timebenchmark.jl
@@ -0,0 +1,90 @@
+using Diversification
+using Distributions
+using ProgressMeter
+using CSV
+using DataFrames
+
+#### load simulated trees
+fnames = Base.Filesystem.readdir("data/simulated_trees")
+
+treefiles = ["data/simulated_trees/" * fname for fname in fnames]
+
+data = Dict()
+for treefile in treefiles
+    phy = readtree(treefile)
+    ntaxa = replace(split(treefile, "/")[3], ".tre" => "")
+    ntaxa = parse(Int64, ntaxa)
+
+    ρ = 1
+    d = make_SSEdata(phy, "", ρ; include_traits = false)
+    data[ntaxa] = d
+end
+
+## Set up the model
+H = 0.587405
+d1 = LogNormal(log(0.29), 3 * H)
+d2 = LogNormal(log(0.20), 3 * H)
+
+n = 6
+speciation = make_quantiles(d1, n)
+extinction = make_quantiles(d2, n)
+
+k = n ^2
+λ = zeros(k)
+μ = zeros(k)
+
+for (i, (sp, ex)) in enumerate(Iterators.product(speciation, extinction))
+    λ[i] = sp
+    μ[i] = ex
+end
+
+## Compute the results
+results = Dict()
+times = Dict()
+@showprogress for (ntaxa, d) in data
+    t1 = time()
+    Ds, Fs = backwards_forwards_pass(model, d; verbose = false)
+    res = calculate_tree_rates(d, model, Ds, Fs; verbose = false);
+    t2 = time()
+    times[ntaxa] = t2 - t1
+
+    results[ntaxa] = res
+end
+
+rdf = CSV.read("output/runtimes_r.csv", DataFrame)
+
+jdf = DataFrame(time = collect(values(times)), ntaxa = collect(keys(times)))
+sort!(jdf, :ntaxa)
+
+ps = []
+for i in 1:2
+    p = plot(xlab = "ntaxa", ylab = "time (seconds)", legend = :bottomright, title = "Arithmetic")
+
+    if i == 1
+        plot!(p, xscale = :log10, yscale = :log10, title = "Log-scale")
+
+        for i in 1:length(rdf[:,:ntaxa])
+            annotate!(p, rdf[i, :ntaxa], rdf[i, :times], text(rdf[:,:ntaxa][i], 6, :left, :top))
+        end
+    end
+    plot!(p, jdf[:, :ntaxa], jdf[:, :time], label = "", color = "black")
+    scatter!(p, jdf[:, :ntaxa], jdf[:, :time], label = "Julia", color = "black")
+
+    ## R
+    plot!(p, rdf[:, :ntaxa], rdf[:, :times], label = "", color = "orange")
+    scatter!(p, rdf[:, :ntaxa], rdf[:, :times], label = "R", color = "orange")
+
+    append!(ps, [p])
+end
+
+runtimeplot = plot(ps...)
+
+savefig(runtimeplot, "figures/runtime_36states_100knotsperbranch.pdf")
+
+
+
+
+
+
+
+
