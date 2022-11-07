7084d396b427d9153fefdcf20228a8785771c84d
Bjorn Tore Kopperud <kopperud@protonmail.com>
2022-11-07 13:40:41 +0100

    Rcpp skeleton

---
+++ BDS/R/BDS.R
@@ -0,0 +1,13 @@
+#' BDS: Birth death shift model
+#'
+#' This package implements the birth-death shift model for diversification
+#' analysis based on reconstructed phylogenies.
+#'
+#' @section BDS functions:
+#' Functions here...
+#'
+#' @docType package
+#' @name BDS
+#' @useDynLib BDS
+#' @importFrom Rcpp sourceCpp
+NULL
