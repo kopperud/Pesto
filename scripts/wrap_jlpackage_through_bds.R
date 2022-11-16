a8990d268d89247d00f65eedf87a0d602d0f673e
Bjorn Tore Kopperud <kopperud@protonmail.com>
2022-11-16 16:34:20 +0100

    .

--- scripts/wrap_jlpackage_through_bds.R
+++ scripts/wrap_jlpackage_through_bds.R
@@ -8,7 +8,8 @@ julia_eval('import Pkg; Pkg.add(url = "https://github.com/kopperud/Diversificati
 
 ## Install the R-package
 ## (will need a working SSH-key since the repository is private)
-remotes::install_git("git@github.com:hoehna/BDS_deterministic_map.git", subdir = "BDS")
+## remotes::install_git("git@github.com:hoehna/BDS_deterministic_map.git", subdir = "BDS")
+remotes::install_local("~/projects/BDS_deterministic_map/BDS")
 
 ## Run the analysis using the R-package wrapper
 library(BDS)
@@ -23,3 +24,25 @@ eta <- 0.05
 
 res <- birth_death_shift3(primates, lambda, mu, eta, rho, verbose = TRUE)
 lambda_average <- res$lambda
+
+
+library(ape)
+library(ggtree)
+library(tidytree)
+library(ggplot2)
+library(dplyr)
+
+
+th <- max(node.depth.edgelength(primates))
+
+df1 <- tibble("node" = 1:max(primates$edge),
+              "Speciation rate" = lambda_average)
+x <- as_tibble(primates)
+
+phydf <- merge(x, df1, by = "node")
+td_phy <- as.treedata(phydf)
+
+p1a <- ggtree(td_phy, aes(color = `Speciation rate`)) +
+  geom_tiplab(size = 8) +
+  theme(legend.position = c(0.2, 0.8)) +
+  xlim(c(0.0, th + 10))
