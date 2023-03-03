13c16d130f017582c65e8a32c2562979ac31e31c
Bjorn Tore Kopperud <kopperud@protonmail.com>
2023-03-03 11:09:37 +0100

    delete everything other than the R package

--- rk4dummy/build.sh
+++
@@ -1,1 +0,0 @@
-g++ -std=c++17 -Wall -pedantic main.cpp -o out -lgsl -lgslcblas -O3 
--- scripts/submit_scm_jobs.sh
+++
@@ -1,4 +0,0 @@
-#-- Launch slurm job array
-echo -e "Launching analyses... "
-sbatch -p lemmium --array="1-$(wc -l scripts/arg_list_scm.txt | cut -d ' ' -f1)" ./scripts/job.sh
-echo "done"
